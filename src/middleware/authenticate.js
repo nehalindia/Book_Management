f4a6b51b98c27779c552d3ba5ad6ea119ae9a766
kazmy <nehaluddin.dpe@gmail.com>
2023-06-23 15:01:57 +0530

    new auth

--- src/middleware/authenticate.js
+++ src/middleware/authenticate.js
@@ -10,24 +10,30 @@ const userVerify = async (req,res, next) => {
     try{
         const token = req.headers["x-api-key"]
         if(!token) {return res.status(401).send({status:false, message: "token is requires!"})}
-        jwt.verify(token, JWT_SECRET_KEY, async (err, decoded) => {
-            if(err) {return res.status(403).send({status:false,message:"Invalid token!"}) }
-            else{       
-                if(!ObjectId.isValid(decoded.userId)) {
-                    res.status(400).send({status: false, message: ` not a valid token id`})
-                    return
-                }
-                const theUser = await userModel.findOne({_id:decoded.userId})
+        let decoded = jwt.verify(token, JWT_SECRET_KEY)
+    //    let decoded = jwt.verify(token, JWT_SECRET_KEY, async (err, decoded) => {
+    //         if(err){return res.status(403).send({status:false,message:"Invalid token!"}) }
+    //         else{       
+    //             if(!ObjectId.isValid(decoded.userId)) {
+    //                 res.status(400).send({status: false, message: ` not a valid token id`})
+    //                 return
+    //             }
+    //             const theUser = await userModel.findOne({_id:decoded.userId})
                 
-                if(!theUser){ return res.status(401).json({status: false, msg: "author not login"})}
-                // console.log(req.body.userId , decoded.userId)
-                // if(req.body.userId !== theUser._id){
-                //     return res.status(401).send({status :false, message: "userId not matched"})
-                // }
-                req.userId = decoded.userId
+    //             if(!theUser){ return res.status(401).json({status: false, msg: "author not login"})}
+    //             // console.log(req.body.userId , decoded.userId)
+    //             // if(req.body.userId !== theUser._id){
+    //             //     return res.status(401).send({status :false, message: "userId not matched"})
+    //             // }
+    //             req.userId = decoded.userId
+    //                 next()
+    //         }
+    //      })
+    // const theUser = await userModel.findOne({_id:decoded.userId})
+                
+                if(!decoded){ return res.status(401).json({status: false, msg: "author not login"})}
+    req.userId = decoded.userId
                     next()
-            }
-         })
     }catch(error){
         res.status(500).send({status:false, message: error.message })
     }
