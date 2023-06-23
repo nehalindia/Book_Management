bddb6ed017864931cacce01a1829718064ead153
kazmy <nehaluddin.dpe@gmail.com>
2023-06-23 18:38:14 +0530

    fixes

--- src/middleware/authenticate.js
+++ src/middleware/authenticate.js
@@ -10,16 +10,16 @@ const userVerify = async (req,res, next) => {
     try{
         const token = req.headers["x-api-key"]
         if(!token) {return res.status(401).send({status:false, message: "token is requires!"})}
-        jwt.verify(token, JWT_SECRET_KEY, (err, decoded) => {
-            if(err){return res.status(403).send({status:false,message:"Invalid token!"}) }
+        jwt.verify(token, JWT_SECRET_KEY, async (err, decoded) => {
+            // if(err){return res.status(403).send({status:false,message:"Invalid token!"}) }
             // else{       
                 // if(!ObjectId.isValid(decoded.userId)) {
                 //     res.status(400).send({status: false, message: ` not a valid token id`})
                 //     return
                 // }
-                // const theUser = await userModel.findOne({_id:decoded.userId})
+                const theUser = await userModel.findOne({_id:decoded.userId})
                 
-                if(!decoded){ return res.status(401).json({status: false, msg: "author not login"})}
+                if(!theUser){ return res.status(401).json({status: false, msg: "author not login"})}
                 // console.log(req.body.userId , decoded.userId)
                 // if(req.body.userId !== theUser._id){
                 //     return res.status(401).send({status :false, message: "userId not matched"})
