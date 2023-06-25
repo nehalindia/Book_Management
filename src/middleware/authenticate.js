1be66558cdda044b2db39a183419785ad27aa3f4
kazmy <nehaluddin.dpe@gmail.com>
2023-06-25 19:38:53 +0530

    fixes

--- src/middleware/authenticate.js
+++ src/middleware/authenticate.js
@@ -1,29 +1,17 @@
 const userModel = require("../models/userModel")
-const mongoose = require('mongoose')
-const ObjectId = mongoose.Types.ObjectId
 const jwt = require('jsonwebtoken');
 require('dotenv').config
 
-const JWT_SECRET_KEY = "secret-key-for-login"
 
 const userVerify = async (req,res, next) => {
     try{
         const token = req.headers["x-api-key"]
         if(!token) {return res.status(401).send({status:false, message: "token is requires!"})}
         jwt.verify(token, process.env.JWT_SECRET_KEY, async (err, decoded) => {
-            if(err){return res.status(403).send({status:false,message:"Invalid token! author"}) }
+            if(err){return res.status(401).send({status:false,message:"Invalid token! author"}) }
             else{       
-                // if(!ObjectId.isValid(decoded.userId)) {
-                //     res.status(400).send({status: false, message: ` not a valid token id`})
-                //     return
-                // }
                 const theUser = await userModel.findOne({_id:decoded.userId})
-                
                 if(!theUser){ return res.status(401).json({status: false, msg: "author not login"})}
-                // console.log(req.body.userId , decoded.userId)
-                // if(req.body.userId !== theUser._id.toString()){
-                //     return res.status(401).send({status :false, message: "userId not matched"})
-                // }
                 req.userId = decoded.userId
                     next()
             }
@@ -34,4 +22,6 @@ const userVerify = async (req,res, next) => {
 }
 
 
-module.exports = {userVerify}
+module.exports = {
+    userVerify
+}
