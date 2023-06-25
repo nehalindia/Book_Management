2a3590f02119085fd184582361ce37bd7f727939
kazmy <nehaluddin.dpe@gmail.com>
2023-06-25 19:32:03 +0530

    fixes

--- src/middleware/authenticate.js
+++ src/middleware/authenticate.js
@@ -11,7 +11,7 @@ const userVerify = async (req,res, next) => {
         const token = req.headers["x-api-key"]
         if(!token) {return res.status(401).send({status:false, message: "token is requires!"})}
         jwt.verify(token, process.env.JWT_SECRET_KEY, async (err, decoded) => {
-            if(err){return res.status(403).send({status:false,message:"Invalid token!"}) }
+            if(err){return res.status(403).send({status:false,message:"Invalid token! author"}) }
             else{       
                 // if(!ObjectId.isValid(decoded.userId)) {
                 //     res.status(400).send({status: false, message: ` not a valid token id`})
