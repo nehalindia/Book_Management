63fd0c1a29a593e1b5476f0afb4ef8206a478259
kazmy <nehaluddin.dpe@gmail.com>
2023-06-27 15:20:02 +0530

    new

--- src/middleware/authenticate.js
+++ src/middleware/authenticate.js
@@ -7,14 +7,14 @@ const userVerify = async (req,res, next) => {
     try{
         const token = req.headers['x-api-key']
         if(!token) {return res.status(401).send({status:false, message: "token is requires!"})}
-        jwt.verify(token, process.env.JWT_SECRET_KEY, async (err, decoded) => {
-            // if(err){return res.status(403).send({status:false,message:"Invalid token! author"}) }
-            // else{       
-                const theUser = await userModel.findOne({_id:decoded.userId})
-                if(!theUser){ return res.status(401).json({status: false, msg: "author not login"})}
+        jwt.verify(token, process.env.JWT_SECRET_KEY, (err, decoded) => {
+            if(err){return res.status(403).send({status:false,message:"Invalid token! author"}) }
+            else{       
+                // const theUser = await userModel.findOne({_id:decoded.userId})
+                // if(!theUser){ return res.status(401).json({status: false, msg: "author not login"})}
                 req.userId = decoded.userId
                     next()
-            // }
+            }
         })
     }catch(error){
         res.status(500).send({status:false, message: error.message })
